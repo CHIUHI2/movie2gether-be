4fbb0a8ef10906487136f34eea21470f0a8cb5ed
Hins Chiu <chiuhi2@oocl.com>
2020-12-12 15:13:34 +0800

    init project

---
+++ src/main/java/com/bootcamp/movie2gether/Movie2getherApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.movie2gether;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Movie2getherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Movie2getherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bootcamp/movie2gether/Movie2getherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.movie2gether;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Movie2getherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
