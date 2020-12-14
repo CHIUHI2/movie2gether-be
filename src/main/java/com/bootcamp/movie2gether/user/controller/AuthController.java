6b1cfc2cdc409ba7590b08f5dec628bfb122ca08
kenckl <klcheung8355@gmail.com>
2020-12-14 15:27:01 +0800

    [User Registration][Ken] Add message response when user attempted to register

--- src/main/java/com/bootcamp/movie2gether/user/controller/AuthController.java
+++ src/main/java/com/bootcamp/movie2gether/user/controller/AuthController.java
@@ -1,5 +1,6 @@
 package com.bootcamp.movie2gether.user.controller;
 
+import com.bootcamp.movie2gether.user.dto.MessageResponse;
 import com.bootcamp.movie2gether.user.dto.RegisterRequest;
 import com.bootcamp.movie2gether.user.dto.RegisterResponse;
 import com.bootcamp.movie2gether.user.entity.User;
@@ -30,18 +31,18 @@ public class AuthController {
         if (userRepository.existsByUserName(registerRequest.getUserName())){
             return ResponseEntity
                     .badRequest()
-                    .body(new RegisterResponse());
+                    .body(new MessageResponse("This username has been used!"));
         }
         if (userRepository.existsByEmail(registerRequest.getEmail())){
             return ResponseEntity
                     .badRequest()
-                    .body(new RegisterResponse());
+                    .body(new MessageResponse("This email has been used!"));
         }
 
         User user = new User(registerRequest.getUserName(), registerRequest.getEmail(), passwordEncoder.encode(registerRequest.getPassword()));
 
         userRepository.save(user);
-        return ResponseEntity.ok(new RegisterResponse());
+        return ResponseEntity.ok(new MessageResponse("Registration successful!"));
 
     }
 
