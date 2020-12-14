8550b9587e85fe74d54da5da8f3cf4cca36eaec5
kenckl <klcheung8355@gmail.com>
2020-12-14 11:07:04 +0800

    [User Registration][Ken] Refactor user entity and user repository

--- src/main/java/com/bootcamp/movie2gether/movie/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.bootcamp.movie2gether.movie.repository;
-
-import com.bootcamp.movie2gether.user.entity.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepository extends MongoRepository<User, String> {
-
-}
