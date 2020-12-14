c537c5f47078e8eacf77b8bf710f381e79c50864
howardchan-oocl <howard.chan@oocl.com>
2020-12-14 16:22:13 +0800

    [movieDetail-33][Howard]feat: implement the test and function of findById for movieService

--- src/main/java/com/bootcamp/movie2gether/movie/service/MovieService.java
+++ src/main/java/com/bootcamp/movie2gether/movie/service/MovieService.java
@@ -1,12 +1,21 @@
 package com.bootcamp.movie2gether.movie.service;
 
+import com.bootcamp.movie2gether.movie.entity.Movie;
 import com.bootcamp.movie2gether.movie.repository.MovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class MovieService {
     @Autowired
     MovieRepository movieRepository;
 
+    public Movie findById(String id) throws Exception {
+        if(!movieRepository.existsById(id)){
+            throw new Exception();
+        }
+        return movieRepository.findById(id).get();
+    }
 }
