1a28fb67fc91c18b67a053252c587375489ea5e1
howardchan-oocl <howard.chan@oocl.com>
2020-12-14 16:22:13 +0800

    [movieDetail-33][Howard]feat: implement the test and function of findById for movieService

--- src/test/java/com/bootcamp/movie2gether/movie/service/MovieServiceTest.java
+++ src/test/java/com/bootcamp/movie2gether/movie/service/MovieServiceTest.java
@@ -1,14 +1,41 @@
 package com.bootcamp.movie2gether.movie.service;
 
+import com.bootcamp.movie2gether.movie.entity.Movie;
 import com.bootcamp.movie2gether.movie.repository.MovieRepository;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 @ExtendWith(MockitoExtension.class)
 public class MovieServiceTest {
     @Mock
     private MovieRepository movieRepository;
+
+    @InjectMocks
+    MovieService movieService;
+
+    @Test
+    public void should_return_movie_detail_when_browse_with_a_valid_id_given_the_movie_detail_exists() throws Exception {
+        //given
+        Movie expected = new Movie();
+
+        when(movieRepository.findById(anyString())).thenReturn(Optional.of(expected));
+        when(movieRepository.existsById(anyString())).thenReturn(true);
+
+        //when
+        Movie movie = movieService.findById("0");
+
+        //then
+        assertEquals(expected, movie);
+    }
 }
