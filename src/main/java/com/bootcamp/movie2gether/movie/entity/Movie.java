5f2771ba8766337f1a079b0a6a0217c1aecd9dc6
Alex Au <alex_acw@outlook.com>
2020-12-14 14:23:27 +0800

    [setup][AlexAu] feat: Add Movie entity matching data in DB

--- src/main/java/com/bootcamp/movie2gether/movie/entity/Movie.java
+++ src/main/java/com/bootcamp/movie2gether/movie/entity/Movie.java
@@ -1,21 +1,24 @@
 package com.bootcamp.movie2gether.movie.entity;
 
+import lombok.Data;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.FieldType;
 import org.springframework.data.mongodb.core.mapping.MongoId;
 
-@Document
+import java.time.ZonedDateTime;
+
+@Document(collation = "movies")
+@Data
 public class Movie {
     @MongoId(FieldType.OBJECT_ID)
     private String id;
-
-    public Movie() {}
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
+    private String overview;
+    private Float popularity;
+    private ZonedDateTime releaseDate;
+    private Integer runtime;
+    private String tagline;
+    private String title;
+    private Float rateAverage;
+    private String posterUrl;
+    private Boolean onShow;
 }
