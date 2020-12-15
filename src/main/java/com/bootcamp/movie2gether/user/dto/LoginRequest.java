f79ade6911a2d1c08c62faacad99859084c60676
kenckl <klcheung8355@gmail.com>
2020-12-15 09:24:26 +0800

    [jwt-auth story][Ken] Implemented jwt, login and register

--- src/main/java/com/bootcamp/movie2gether/user/dto/LoginRequest.java
+++ src/main/java/com/bootcamp/movie2gether/user/dto/LoginRequest.java
@@ -1,4 +1,29 @@
 package com.bootcamp.movie2gether.user.dto;
 
 public class LoginRequest {
+    private String userName;
+    private String password;
+
+    public LoginRequest(String userName, String password){
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public LoginRequest(){}
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUsername(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
