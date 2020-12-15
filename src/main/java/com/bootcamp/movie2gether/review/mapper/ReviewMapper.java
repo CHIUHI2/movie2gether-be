67ee569435d640bb20d6b89833d8454265e245fe
davidltk123 <davidltk123@gmail.com>
2020-12-15 22:01:36 +0800

    [rating-40][david] feat: add mapper

--- src/main/java/com/bootcamp/movie2gether/review/mapper/ReviewMapper.java
+++ src/main/java/com/bootcamp/movie2gether/review/mapper/ReviewMapper.java
@@ -1,7 +1,22 @@
 package com.bootcamp.movie2gether.review.mapper;
 
+import com.bootcamp.movie2gether.review.dto.ReviewRequest;
+import com.bootcamp.movie2gether.review.dto.ReviewResponse;
+import com.bootcamp.movie2gether.review.entity.Review;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ReviewMapper {
+    public Review toEntity(ReviewRequest reviewRequest){
+        Review review = new Review();
+        BeanUtils.copyProperties(reviewRequest, review);
+        return review;
+    }
+
+    public ReviewResponse toResponse(Review review){
+        ReviewResponse reviewResponse = new ReviewResponse();
+        BeanUtils.copyProperties(review, reviewResponse);
+        return reviewResponse;
+    }
 }
