acd9ce38e599989690f9a08fb481ec6dce8a73a0
Alex Au <alex_acw@outlook.com>
2020-12-16 01:27:52 +0800

    [34][AlexAu] test: should_return_paged_unique_cinemas_when_get_cinema_by_movie_given_movie

--- src/test/java/com/bootcamp/movie2gether/movie/integration/CinemaIntegrationTest.java
+++ src/test/java/com/bootcamp/movie2gether/movie/integration/CinemaIntegrationTest.java
@@ -1,7 +1,9 @@
 package com.bootcamp.movie2gether.movie.integration;
 
 import com.bootcamp.movie2gether.movie.entity.Cinema;
+import com.bootcamp.movie2gether.movie.entity.Movie;
 import com.bootcamp.movie2gether.movie.entity.Seat;
+import com.bootcamp.movie2gether.movie.entity.Session;
 import com.bootcamp.movie2gether.movie.repository.BookingRepository;
 import com.bootcamp.movie2gether.movie.repository.CinemaRepository;
 import com.bootcamp.movie2gether.movie.repository.MovieRepository;
@@ -81,6 +83,41 @@ public class CinemaIntegrationTest {
                 .andExpect(jsonPath("$.content[0].name").value(cinema2.getName()))
                 .andExpect(jsonPath("$.content[0].seats", hasSize(10)))
                 .andExpect(jsonPath("$.totalElements").value(2))
+        ;
+        //then
+    }
+
+    @Test
+    void should_return_paged_unique_cinemas_when_get_cinema_by_movie_given_movie() throws Exception {
+        //given
+        Cinema cinema1 = cinemaRepository.insert(
+                Cinema.builder()
+                        .seats(
+                                IntStream.range(0, 10)
+                                        .mapToObj(i -> new Seat(String.format("A%d", i)))
+                                        .collect(Collectors.toList()))
+                        .name("1111")
+                        .build()
+        );
+        Cinema cinema2 = cinemaRepository.insert(
+                Cinema.builder()
+                        .seats(
+                                IntStream.range(0, 10)
+                                        .mapToObj(i -> new Seat(String.format("A%d", i)))
+                                        .collect(Collectors.toList()))
+                        .name("2222")
+                        .build()
+        );
+        Movie movie = movieRepository.insert(Movie.builder().title("mmm").build());
+        sessionRepository.insert(Session.builder().cinemaId(cinema1.getId()).movieId(movie.getId()).build());
+        //when
+        mockMvc.perform(get(String.format("%s?page=%d&pageSize=%d&movieId=%s", BOOKINGS_PATH, 0, 1, movie.getId().toHexString())))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.content", hasSize(1)))
+                .andExpect(jsonPath("$.content[0].id").value(cinema1.getId().toHexString()))
+                .andExpect(jsonPath("$.content[0].name").value(cinema1.getName()))
+                .andExpect(jsonPath("$.content[0].seats", hasSize(10)))
+                .andExpect(jsonPath("$.totalElements").value(1))
         ;
         //then
     }
