b7bbc67dade8fddf4c97df07eeb05886de8fabf7
Alex Au <alex_acw@outlook.com>
2020-12-15 15:52:18 +0800

    [34][AlexAu] test: should_return_booking_with_session_detail_when_get_booking_with_userId

--- src/test/java/com/bootcamp/movie2gether/movie/integration/BookingIntegrationTest.java
+++ src/test/java/com/bootcamp/movie2gether/movie/integration/BookingIntegrationTest.java
@@ -1,11 +1,7 @@
 package com.bootcamp.movie2gether.movie.integration;
 
 import com.bootcamp.movie2gether.movie.dto.BookingRequest;
-import com.bootcamp.movie2gether.movie.entity.Cinema;
-import com.bootcamp.movie2gether.movie.entity.Movie;
-import com.bootcamp.movie2gether.movie.entity.Seat;
-import com.bootcamp.movie2gether.movie.entity.Session;
-import com.bootcamp.movie2gether.movie.exception.AlreadyBookedException;
+import com.bootcamp.movie2gether.movie.entity.*;
 import com.bootcamp.movie2gether.movie.repository.BookingRepository;
 import com.bootcamp.movie2gether.movie.repository.CinemaRepository;
 import com.bootcamp.movie2gether.movie.repository.MovieRepository;
@@ -26,11 +22,11 @@ import org.springframework.test.web.servlet.MockMvc;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.startsWith;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -90,12 +86,49 @@ public class BookingIntegrationTest {
                         ))
                 .andExpect(status().isCreated())
                 .andExpect(jsonPath("$.id").isString())
+//                TODO: check user id as auth is not implemented yet
 //                .andExpect(jsonPath("$.userId").value(user.getId().toHexString()))
                 .andExpect(jsonPath("$.userId").isString())
                 .andExpect(jsonPath("$.sessionId").value(session.getId().toHexString()))
                 .andExpect(jsonPath("$.seatNumber").value("A2"))
         ;
+    }
 
+    @Test
+    @WithMockUser(value = "spring")
+    void should_return_booking_with_session_detail_when_get_booking_with_userId() throws Exception {
+        //given
+        Movie movie = movieRepository.save(Movie.builder().onShow(true).title("Tenet").build());
+        Cinema cinema = cinemaRepository.save(Cinema.builder().seats(IntStream.range(0, 10)
+                .mapToObj(i -> new Seat(String.format("A%d", i)))
+                .collect(Collectors.toList()))
+                .name("AAAA")
+                .build()
+        );
+        LocalDateTime startTime = LocalDateTime.now();
+        LocalDateTime endTime = LocalDateTime.now().plus(Duration.ofHours(2));
+        Session session = sessionRepository.save(Session.builder().cinemaId(cinema.getId()).movieId(movie.getId()).startTime(startTime).endTime(endTime).build());
+        User user = userRepository.save(new User("spring", "spring@mail.com", "lmao"));
+        Booking booking = bookingService.book(user.getId(), session.getId(), "A2");
+        //when
+        //then
+        mockMvc.perform(
+                get(String.format("/bookings&$page=0&pageSize=10&userId=%s", user.getId().toHexString()))
+        )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.content", hasSize(1)))
+                .andExpect(jsonPath("$.content.[0].id").value(booking.getId().toHexString()))
+                .andExpect(jsonPath("$.content.[0].seatNumber").value(booking.getSeatNumber()))
+                .andExpect(jsonPath("$.content.[0].userId").value(user.getId().toHexString()))
+                .andExpect(jsonPath("$.content.[0].session.movie.id").value(movie.getId().toHexString()))
+                .andExpect(jsonPath("$.content.[0].session.movie.title").value(movie.getTitle()))
+                .andExpect(jsonPath("$.content.[0].session.cinema.id").value(cinema.getId().toHexString()))
+                .andExpect(jsonPath("$.content.[0].session.cinema.name").value(cinema.getName()))
+                .andExpect(jsonPath("$.content.[0].session.cinema.seats", hasSize(cinema.getSeats().size())))
+                .andExpect(jsonPath("$.content.[0].session.bookings", hasSize(1)))
+                .andExpect(jsonPath("$.content.[0].session.startTime", startsWith(startTime.format(DateTimeFormatter.ofPattern("uuuu-MM-dd'T'HH:mm:ss.SSS")))))
+                .andExpect(jsonPath("$.content.[0].session.endTime", startsWith(endTime.format(DateTimeFormatter.ofPattern("uuuu-MM-dd'T'HH:mm:ss.SSS")))))
+        ;
     }
 
 
