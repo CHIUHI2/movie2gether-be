21304d1277b31b6f4a844182b48c58fcb9b4d629
davidltk123 <davidltk123@gmail.com>
2020-12-15 23:15:41 +0800

    [rating-40][david] feat: supplment getByMovieIdAndUserId feature

--- src/main/java/com/bootcamp/movie2gether/review/service/ReviewService.java
+++ src/main/java/com/bootcamp/movie2gether/review/service/ReviewService.java
@@ -2,8 +2,11 @@ package com.bootcamp.movie2gether.review.service;
 
 import com.bootcamp.movie2gether.review.entity.Review;
 import com.bootcamp.movie2gether.review.repository.ReviewRepository;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ReviewService {
@@ -11,6 +14,7 @@ public class ReviewService {
     ReviewRepository reviewRepository;
 
     public Review getByMovieIdAndUserId(String movieId, String userId) {
-        return null;
+        return reviewRepository.findByMovieIdAndUserId(new ObjectId(movieId),new ObjectId(userId));
     }
+
 }
