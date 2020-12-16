9a9d676f087b81371775074c2967747919f2746f
Alex Au <alex_acw@outlook.com>
2020-12-16 23:04:14 +0800

    [61][AlexAu] test: should_no_new_booking_created_when_book_seats_given_some_seats_already_booked

--- src/test/java/com/bootcamp/movie2gether/movie/service/BookingServiceTest.java
+++ src/test/java/com/bootcamp/movie2gether/movie/service/BookingServiceTest.java
@@ -15,7 +15,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.mongodb.core.MongoTemplate;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -41,6 +43,9 @@ public class BookingServiceTest {
         sessionRepository.deleteAll();
     }
 
+    @Autowired
+    MongoTemplate mongoTemplate;
+
     @Test
     void should_store_and_return_booking_when_book_ticket_given_session_seat_and_user() throws AlreadyBookedException {
         //given
@@ -58,7 +63,7 @@ public class BookingServiceTest {
         sessionRepository.save(session);
 
         //when
-        Booking actual = bookingService.book(user.getId(), session.getId(), "A1");
+        Booking actual = bookingService.book(user.getId(), session.getId(), Collections.singletonList("A1")).get(0);
 
         //expect
         List<Booking> storedBookings = bookingRepository.findAll();
@@ -89,11 +94,34 @@ public class BookingServiceTest {
         sessionRepository.save(session);
 
         //when
-        bookingService.book(userBooked.getId(), session.getId(), "A1");
+        bookingService.book(userBooked.getId(), session.getId(), Collections.singletonList("A1"));
         //expect
-        assertThrows(AlreadyBookedException.class, () -> bookingService.book(user.getId(), session.getId(), "A1"));
+        assertThrows(AlreadyBookedException.class, () -> bookingService.book(user.getId(), session.getId(), Collections.singletonList("A1")));
         assertEquals(1, bookingRepository.count());
+    }
 
+    @Test
+    void should_no_new_booking_created_when_book_seats_given_some_seats_already_booked() throws AlreadyBookedException {
+        //given
+        BookingService bookingService = new BookingService(bookingRepository, userRepository, sessionRepository);
 
+        Cinema cinema = new Cinema();
+        Seat seat = new Seat();
+        seat.setNumber("A1");
+        cinema.setSeats(Collections.singletonList(seat));
+        cinema = cinemaRepository.save(cinema);
+
+        User userBooked = userRepository.save(new User());
+        User user = userRepository.save(new User());
+
+        Session session = new Session();
+        session.setCinemaId(cinema.getId());
+        sessionRepository.save(session);
+
+        //when
+        bookingService.book(userBooked.getId(), session.getId(), Collections.singletonList("A1"));
+        //expect
+        assertThrows(AlreadyBookedException.class, () -> bookingService.book(user.getId(), session.getId(), Arrays.asList("A1", "A2")));
+        assertEquals(1, bookingRepository.count());
     }
 }
