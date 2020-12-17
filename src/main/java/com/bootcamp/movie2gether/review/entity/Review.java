4d836bf69b9854c89402423e992cbad5819423f5
Hins Chiu <chiuhi2@oocl.com>
2020-12-17 14:12:30 +0800

    [review][Hins]feat:add createdAt and lastModifiedAt

--- src/main/java/com/bootcamp/movie2gether/review/entity/Review.java
+++ src/main/java/com/bootcamp/movie2gether/review/entity/Review.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.mongodb.core.mapping.Document;
 import lombok.Builder;
@@ -13,6 +14,7 @@ import org.springframework.data.mongodb.core.mapping.FieldType;
 import org.springframework.data.mongodb.core.mapping.MongoId;
 
 import java.time.ZonedDateTime;
+import java.util.Date;
 
 @Document
 @Data
@@ -29,6 +31,8 @@ public class Review {
     private ObjectId movieId;
     private Integer rating;
     private String comment;
+    @CreatedDate
+    private Date createdAt;
     @LastModifiedDate
-    private ZonedDateTime lastModifiedAt;
+    private Date lastModifiedAt;
 }
